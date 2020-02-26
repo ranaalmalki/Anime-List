14f419bf859cfc16ac825fa5886c8d11d8c276f5
ranaalmalki <ranaalmalki12@gmail.com>
2020-02-26 16:16:35 +0300

    Updates

---
+++ precache-manifest.ec73c0e9aae157a86e757a9013491747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71fe2bc0949117b505c155a471e4d27",
+    "url": "/Anime-List/index.html"
+  },
+  {
+    "revision": "3e477afb1e0c3d860b9e",
+    "url": "/Anime-List/static/css/main.ae92b67d.chunk.css"
+  },
+  {
+    "revision": "aba76e46c97a071f550b",
+    "url": "/Anime-List/static/js/2.dabfaedc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Anime-List/static/js/2.dabfaedc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e477afb1e0c3d860b9e",
+    "url": "/Anime-List/static/js/main.42e6646d.chunk.js"
+  },
+  {
+    "revision": "acf63dfa8ca5bc598355",
+    "url": "/Anime-List/static/js/runtime-main.35abeb88.js"
+  }
+]);
